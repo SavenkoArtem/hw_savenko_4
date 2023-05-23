628c358a9fa51ee43ce517bdf2f448365a9fd963
ArtemUb <savenkoam@gmail.com>
2023-05-24 08:50:22 +0900

    add scrin

---
+++ task25/Program.cs
@@ -0,0 +1,25 @@
+﻿int Promt(string msg)
+{    
+    Console.WriteLine(msg);
+    int num = Convert.ToInt32(Console.ReadLine());    
+    return num;
+}
+
+
+int NumberOfDegree(int A, int B)
+{
+    int result = Convert.ToInt32(Math.Pow(A, B));
+    return result;
+}
+
+
+try
+{
+int A = Promt("Enter a nutural number:");
+int B = Promt("Enter a degree:");
+Console.WriteLine($"Result = {NumberOfDegree(A, B)}");
+}
+catch
+{
+    Console.WriteLine("You need enter a natural number!");
+}
