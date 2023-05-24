439070931d5bcf718be63de138dfc6418eeb51e9
ArtemUb <savenkoam@gmail.com>
2023-05-24 15:45:25 +0900

    Task 29 is Done

---
+++ task27/Program.cs
@@ -0,0 +1,30 @@
+﻿int Promt(string msg)
+{    
+    Console.WriteLine(msg);
+    int num = Convert.ToInt32(Console.ReadLine());    
+    return num;
+}
+
+
+int SumDigitNumber(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    
+    return sum;
+}
+
+
+try
+{
+int num = Promt("Enter a number:");
+Console.WriteLine($"Result = {SumDigitNumber(num)}");
+}
+catch
+{
+    Console.WriteLine("You need enter a natural number!");
+}
